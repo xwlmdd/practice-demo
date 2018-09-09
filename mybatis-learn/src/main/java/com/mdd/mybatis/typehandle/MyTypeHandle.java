68441807f3c72d2ff748bb6299ecec5f36d93b93
xuwanglu <1072552712@qq.com>
2018-09-09 18:40:54 +0800

    增加加密工具类

--- mybatis-learn/src/main/java/com/mdd/mybatis/typehandle/MyTypeHandle.java
+++ mybatis-learn/src/main/java/com/mdd/mybatis/typehandle/MyTypeHandle.java
@@ -1,5 +1,6 @@
 package com.mdd.mybatis.typehandle;
 
+import com.mdd.mybatis.util.DESUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.TypeHandler;
@@ -10,18 +11,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MyTypeHandle implements TypeHandler<String> {
-    private static String prifix = "mdd";
+    private static String KEY = "123456";
 
     @Override
     public void setParameter(PreparedStatement preparedStatement, int i, String s, JdbcType jdbcType) throws SQLException {
-        preparedStatement.setString(i, s);
+        try {
+            String encrypt = DESUtil.encrypt(s, KEY);
+            preparedStatement.setString(i, encrypt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getResult(ResultSet resultSet, String s) throws SQLException {
         String result = resultSet.getString(s);
         if (StringUtils.isNotEmpty(result)) {
-            return prifix.concat(result);
+            try {
+                return DESUtil.decrypt(result, KEY);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
@@ -30,7 +40,11 @@ public class MyTypeHandle implements TypeHandler<String> {
     public String getResult(ResultSet resultSet, int i) throws SQLException {
         String result = resultSet.getString(i);
         if (StringUtils.isNotEmpty(result)) {
-            return prifix.concat(result);
+            try {
+                return DESUtil.decrypt(result, KEY);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
@@ -39,7 +53,11 @@ public class MyTypeHandle implements TypeHandler<String> {
     public String getResult(CallableStatement callableStatement, int i) throws SQLException {
         String result = callableStatement.getString(i);
         if (StringUtils.isNotEmpty(result)) {
-            return prifix.concat(result);
+            try {
+                return DESUtil.decrypt(result, KEY);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
